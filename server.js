d6bd17f3f0a525e784720d056a02160381f5f453
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-05-08 01:22:09 -0400

    server refactor
    - replaces `http-server` with `express`
    - injects a `beforeunload` listener to kill the server after the client is closed

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
-const { createServer } = require('http-server');
-const open  = require('open');
-const pkg  = require('./package.json');
-const server = createServer({
-	cors: true,
-	root: __dirname + '/app',
-});
-const host = 'localhost';
-const port = 0;
-server.listen(port, host, undefined, function() {
-	const url = `http://${host}:${this.address().port}/index.html`;
-	console.log(`${pkg.name} running on ${url}`);
-	open(url);
+const portfinder = require('portfinder');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const pkg = require('./package.json');
+
+async function main() {
+	const port =
+		process.env.PORT ||
+		(await portfinder.getPortPromise({
+			port: 8000,
+		}));
+	const app = express();
+	let server;
+
+	// send index with injected close script
+	app.get('/', function (_req, res) {
+		fs.readFile(path.join(__dirname, 'app', 'index.html'), { encoding: 'utf-8' }, function (err, data) {
+			if (err) {
+				console.error(err);
+				res.sendStatus(404);
+			} else {
+				res.send(
+					data.replace(
+						'</head>',
+						`<script>
+	window.addEventListener('beforeunload', function () {fetch('./close')});
+</script>
+</head>`
+					)
+				);
+			}
+		});
+	});
+	// handle close request
+	app.get('/close', (_req, res) => {
+		res.sendStatus(200);
+		server.close();
+		process.exit(0);
+	});
+	// host everything else as static assets
+	app.use(express.static(path.join(__dirname, 'app')));
+
+	server = app.listen(port, () => {
+		const url = `http://localhost:${port}`;
+		const open = require('open');
+		open(url);
+	});
+}
+
+main().catch(err => {
+	console.error(err);
+	process.exit(1);
 });
